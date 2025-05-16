7061b77716c9c3dae3a35727452ba26bcb457975
guirochaluz <157293262+guirochaluz@users.noreply.github.com>
2025-05-16 12:31:07 -0300

    Update api.py

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 
 from auth.oauth import get_auth_url, exchange_code, renovar_access_token
-from ml.sales import get_sales  # sales.py em ml/ define get_sales
+from ml.sales import get_full_sales as get_sales
 
 # Carrega variáveis de ambiente
 load_dotenv()
