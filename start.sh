799fc3143663b15ecfa4f40d976931cf9f408ff0
guirochaluz <157293262+guirochaluz@users.noreply.github.com>
2025-05-30 10:36:38 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Inicia FastAPI na porta 8501 (em segundo plano)
+# Inicia o FastAPI em segundo plano na porta 8501
 uvicorn api:app --host 0.0.0.0 --port 8501 &
 
-# Inicia Streamlit na porta 8000 (será a pública)
+# Inicia o Streamlit como serviço principal (na porta 8000, visível)
 streamlit run app.py --server.port 8000 --server.address=0.0.0.0 --server.enableXsrfProtection false
