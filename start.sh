78e54b68b34a0a01b9526673e63358efb73ffc23
guirochaluz <157293262+guirochaluz@users.noreply.github.com>
2025-05-30 10:02:20 -0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Inicia FastAPI na porta 8501 (em segundo plano)
+uvicorn api:app --host 0.0.0.0 --port 8501 &
+
+# Inicia Streamlit na porta 8000 (será a pública)
+streamlit run app.py --server.port 8000 --server.address=0.0.0.0 --server.enableXsrfProtection false
